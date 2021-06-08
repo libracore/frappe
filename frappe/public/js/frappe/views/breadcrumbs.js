e455290d820ad193addfe18f637adefa1c5e941d
lasalesi <larsericmueller@gmail.com>
2021-06-08 12:15:41 +0200

    bugfix: rename error message

--- frappe/public/js/frappe/views/breadcrumbs.js
+++ frappe/public/js/frappe/views/breadcrumbs.js
@@ -127,6 +127,7 @@ frappe.breadcrumbs = {
 		var new_route_str = ["Form", doctype, new_name].join("/");
 		frappe.breadcrumbs.all[new_route_str] = frappe.breadcrumbs.all[old_route_str];
 		delete frappe.breadcrumbs.all[old_route_str];
+		this.update();
 	}
 
 }
