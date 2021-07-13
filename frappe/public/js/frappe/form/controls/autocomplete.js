af56ef1a9f8cd289006894e4f183a388ec067a5b
lasalesi <larsericmueller@gmail.com>
2021-07-13 17:39:30 +0200

    Update autocomplete.js
    bugfix awesomeplete

--- frappe/public/js/frappe/form/controls/autocomplete.js
+++ frappe/public/js/frappe/form/controls/autocomplete.js
@@ -100,18 +100,12 @@ frappe.ui.form.ControlAutocomplete = frappe.ui.form.ControlData.extend({
 	},
 
 	validate(value) {
-		if (this.df.ignore_validation) {
-            return value || '';
-        }
-        let valid_values = this.awesomeplete._list.map(d => d.value);
-        if (!valid_values.length) {
-            return value;
-        }
-        if (valid_values.includes(value)) {
-            return value;
-        } else {
-            return '';
-        }
+		let valid_values = this.awesomplete._list.map(d => d.value);
+		if (valid_values.includes(value)) {
+			return value;
+		} else {
+			return '';
+		}
 	},
 
 	get_data() {
