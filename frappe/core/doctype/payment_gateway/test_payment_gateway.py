a74ecd17a6d710ddcb5df7c8aaca1e422664e7fe
lasalesi <larsericmueller@gmail.com>
2024-11-01 22:11:33 +0100

    adding payment gateways

---
+++ frappe/core/doctype/payment_gateway/test_payment_gateway.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+import unittest
+
+import frappe
+
+# test_records = frappe.get_test_records('Payment Gateway')
+
+
+class TestPaymentGateway(unittest.TestCase):
+	pass
