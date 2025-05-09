fd046008d4206f31281a264bc1d10f48bcd36c26
lasalesi <lars.mueller@libracore.com>
2025-05-09 20:48:57 +0200

    adding gateway controller

--- frappe/integrations/utils.py
+++ frappe/integrations/utils.py
@@ -113,3 +113,14 @@ def get_json(obj):
 def json_handler(obj):
 	if isinstance(obj, datetime.date | datetime.timedelta | datetime.datetime):
 		return str(obj)
+
+def create_payment_gateway(gateway, settings=None, controller=None):
+	# NOTE: we don't translate Payment Gateway name because it is an internal doctype
+	if not frappe.db.exists("Payment Gateway", gateway):
+		payment_gateway = frappe.get_doc({
+			"doctype": "Payment Gateway",
+			"gateway": gateway,
+			"gateway_settings": settings,
+			"gateway_controller": controller
+		})
+		payment_gateway.insert(ignore_permissions=True)
