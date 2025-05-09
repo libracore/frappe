3980858232242083bd9bd4522105737cff55cf4c
lasalesi <lars.mueller@libracore.com>
2025-05-09 22:26:31 +0200

    blue navbar

--- frappe/hooks.py
+++ frappe/hooks.py
@@ -7,7 +7,7 @@ app_title = "Frappe Framework"
 app_publisher = "Frappe Technologies"
 app_description = "Full stack web framework with Python, Javascript, MariaDB, Redis, Node"
 app_license = "MIT"
-app_logo_url = "/assets/frappe/images/frappe-framework-logo.svg"
+app_logo_url = "/assets/frappe/images/libracore.png"
 develop_version = "15.x.x-develop"
 
 app_email = "developers@frappe.io"
