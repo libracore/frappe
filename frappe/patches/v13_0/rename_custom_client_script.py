84ddd9f08fa9cf95574aaf81482673a1001d0401
lasalesi <larsericmueller@gmail.com>
2025-07-24 14:41:44 +0200

    improved patches for gokuflyn

--- frappe/patches/v13_0/rename_custom_client_script.py
+++ frappe/patches/v13_0/rename_custom_client_script.py
@@ -3,11 +3,15 @@ from frappe.model.rename_doc import rename_doc
 
 
 def execute():
-	if frappe.db.exists("DocType", "Client Script"):
-		return
+    if frappe.db.exists("DocType", "Client Script"):
+        return
 
-	frappe.flags.ignore_route_conflict_validation = True
-	rename_doc("DocType", "Custom Script", "Client Script")
-	frappe.flags.ignore_route_conflict_validation = False
+    # cleanup relics in case of an imported DB
+    frappe.db.sql("""DROP TABLE `tabClient Script`;""")
+    frappe.db.sql("""DELETE FROM `tabDocType` WHERE `name` = "Client Script";""")
+    
+    frappe.flags.ignore_route_conflict_validation = True
+    rename_doc("DocType", "Custom Script", "Client Script")
+    frappe.flags.ignore_route_conflict_validation = False
 
-	frappe.reload_doctype("Client Script", force=True)
+    frappe.reload_doctype("Client Script", force=True)
--- frappe/patches/v13_0/rename_desk_page_to_workspace.py
+++ frappe/patches/v13_0/rename_desk_page_to_workspace.py
@@ -3,20 +3,29 @@ from frappe.model.rename_doc import rename_doc
 
 
 def execute():
-	if frappe.db.exists("DocType", "Desk Page"):
-		if frappe.db.exists("DocType", "Workspace"):
-			# this patch was not added initially, so this page might still exist
-			frappe.delete_doc("DocType", "Desk Page")
-		else:
-			frappe.flags.ignore_route_conflict_validation = True
-			rename_doc("DocType", "Desk Page", "Workspace")
-			frappe.flags.ignore_route_conflict_validation = False
+    if frappe.db.exists("DocType", "Desk Page"):
+        if frappe.db.exists("DocType", "Workspace"):
+            # this patch was not added initially, so this page might still exist
+            frappe.delete_doc("DocType", "Desk Page")
+        else:
+            frappe.flags.ignore_route_conflict_validation = True
+            rename_doc("DocType", "Desk Page", "Workspace")
+            frappe.flags.ignore_route_conflict_validation = False
 
-	rename_doc("DocType", "Desk Chart", "Workspace Chart", ignore_if_exists=True)
-	rename_doc("DocType", "Desk Shortcut", "Workspace Shortcut", ignore_if_exists=True)
-	rename_doc("DocType", "Desk Link", "Workspace Link", ignore_if_exists=True)
+    if frappe.db.exists("DocType", "Desk Chart"):
+        rename_doc("DocType", "Desk Chart", "Workspace Chart", ignore_if_exists=True)
+    else:
+        frappe.reload_doc("desk", "doctype", "workspace_chart", force=True)
+    if frappe.db.exists("DocType", "Desk Shortcut"):
+        rename_doc("DocType", "Desk Shortcut", "Workspace Shortcut", ignore_if_exists=True)
+    else:
+        frappe.reload_doc("desk", "doctype", "workspace_shortcut", force=True)
+    if frappe.db.exists("DocType", "Desk Link"):
+        rename_doc("DocType", "Desk Link", "Workspace Link", ignore_if_exists=True)
+    else:
+        frappe.reload_doc("desk", "doctype", "workspace_link", force=True)
 
-	frappe.reload_doc("desk", "doctype", "workspace", force=True)
-	frappe.reload_doc("desk", "doctype", "workspace_link", force=True)
-	frappe.reload_doc("desk", "doctype", "workspace_chart", force=True)
-	frappe.reload_doc("desk", "doctype", "workspace_shortcut", force=True)
+    frappe.reload_doc("desk", "doctype", "workspace", force=True)
+    frappe.reload_doc("desk", "doctype", "workspace_link", force=True)
+    frappe.reload_doc("desk", "doctype", "workspace_chart", force=True)
+    frappe.reload_doc("desk", "doctype", "workspace_shortcut", force=True)
