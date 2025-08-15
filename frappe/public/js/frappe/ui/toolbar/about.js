848c2a411f2844ba90ee28a9a9a2aaaba243c91c
lasalesi <larsericmueller@gmail.com>
2025-08-15 16:32:21 +0200

    updating about resources

--- frappe/public/js/frappe/ui/toolbar/about.js
+++ frappe/public/js/frappe/ui/toolbar/about.js
@@ -9,23 +9,17 @@ frappe.ui.misc.about = function () {
 					<p>${__("Open Source Applications for the Web")}</p>
 					<p><i class='fa fa-globe fa-fw'></i>
 						${__("Website")}:
-						<a href='https://frappeframework.com' target='_blank'>https://frappeframework.com</a></p>
+						<a href='https://www.libracore.com' target='_blank'>https://www.libracore.com</a></p>
 					<p><i class='fa fa-github fa-fw'></i>
 						${__("Source")}:
-						<a href='https://github.com/frappe' target='_blank'>https://github.com/frappe</a></p>
-					<p><i class='fa fa-graduation-cap fa-fw'></i>
-						Frappe School: <a href='https://frappe.school' target='_blank'>https://frappe.school</a></p>
+						<a href='https://git.libracore.io/libracore' target='_blank'>https://git.libracore.io/libracore</a></p>
 					<p><i class='fa fa-linkedin fa-fw'></i>
-						Linkedin: <a href='https://linkedin.com/company/frappe-tech' target='_blank'>https://linkedin.com/company/frappe-tech</a></p>
-					<p><i class='fa fa-twitter fa-fw'></i>
-						Twitter: <a href='https://twitter.com/frappetech' target='_blank'>https://twitter.com/frappetech</a></p>
-					<p><i class='fa fa-youtube fa-fw'></i>
-						YouTube: <a href='https://www.youtube.com/@frappetech' target='_blank'>https://www.youtube.com/@frappetech</a></p>
+						Linkedin: <a href='https://linkedin.com/company/libracore' target='_blank'>https://linkedin.com/company/libracore</a></p>
 					<hr>
 					<h4>${__("Installed Apps")}</h4>
 					<div id='about-app-versions'>${__("Loading versions...")}</div>
 					<hr>
-					<p class='text-muted'>${__("&copy; Frappe Technologies Pvt. Ltd. and contributors")} </p>
+					<p class='text-muted'>${__("&copy; libracore, Frappe Technologies Pvt. Ltd. and contributors")} </p>
 					</div>`,
 				frappe.app
 			)
